2f3c48409e318d854ea66b59017b11e71587e27c
sooziini <soojinpar1026@gmail.com>
2021-04-12 17:18:18 +0900

    initial settings

---
+++ capstone/app/src/main/java/com/example/capstone/BoardWriteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+    }
+}

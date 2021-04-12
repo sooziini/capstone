195eb07c8ee851ef7f1ebd61d2daad3ba896f36e
sooziini <soojinpar1026@gmail.com>
2021-04-12 16:16:07 +0900

    initial settings

---
+++ capstone/app/src/main/java/com/example/capstone/BoardListActivity.kt
@@ -0,0 +1,12 @@
+
+package com.example.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_list)
+    }
+}

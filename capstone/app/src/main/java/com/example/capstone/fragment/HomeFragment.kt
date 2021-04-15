8d30f672a197a9af73214b4f4d4cd772af6d77d2
sooziini <soojinpar1026@gmail.com>
2021-04-16 00:07:57 +0900

    modify bottomnavigationview (add selector)

--- capstone/app/src/main/java/com/example/capstone/fragment/HomeFragment.kt
+++ capstone/app/src/main/java/com/example/capstone/fragment/HomeFragment.kt
@@ -7,17 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.capstone.FreeBoardActivity
 import com.example.capstone.R
-import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.support.v4.startActivity
 
-class MainFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {

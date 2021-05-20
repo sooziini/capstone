ae7094f755b3a12b3e742fdeafce6850cae751cb
sooziini <soojinpar1026@gmail.com>
2021-05-21 01:16:40 +0900

    SearchActivity 수정 및 keyboard hide 설정

--- capstone/app/src/main/java/com/example/capstone/BoardWriteActivity.kt
+++ capstone/app/src/main/java/com/example/capstone/BoardWriteActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_board_write.*
 import org.jetbrains.anko.toast
 import retrofit2.Call
@@ -11,6 +13,10 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class BoardWriteActivity : AppCompatActivity() {
+
+    // 키보드 InputMethodManager 변수 선언
+    var imm: InputMethodManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write)
@@ -19,6 +25,9 @@ class BoardWriteActivity : AppCompatActivity() {
         setSupportActionBar(board_write_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)       // 기본 뒤로가기 버튼 설정
         supportActionBar?.setDisplayShowTitleEnabled(false)     // 기본 title 제거
+
+        // 키보드 InputMethodManager 세팅
+        imm = getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as InputMethodManager?
 
         // 글쓰기 완료 버튼을 클릭했을 경우
         board_write_btn.setOnClickListener {
@@ -68,5 +77,13 @@ class BoardWriteActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    // 이벤트 메서드 생성
+    // 액티비티 최상위 layout에 onClick 세팅
+    // 해당 layout 내 view 클릭 시 함수 실행
+    fun hideKeyboard(v: View) {
+        if (v != null)
+            imm?.hideSoftInputFromWindow(v.windowToken, 0)
     }
 }
